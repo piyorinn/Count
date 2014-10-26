9add8b86c0a65494c1ce2f8b2f390c9337db1706
Nakazawa Namiko <nakazawa@Macintosh.local>
2014-10-26 14:10:29 +0900

    Initial Commit

---
+++ Count/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Count
+//
+//  Created by Nakazawa Namiko on 2014/10/26.
+//  Copyright (c) 2014年 Nakazawa Yuri. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
